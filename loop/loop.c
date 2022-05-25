1368cfee9438a3cf0ae4fb42fa7308ac90a68bb2
unknown <cecilofori1@gmail.com>
2022-05-25 15:35:56 +0000

    Initial commit

---
+++ loop/loop.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main()
+{
+
+	int i = 11;
+
+	int dest = 37;
+
+	printf("Numbers from 11 to 37");
+
+	while ( 11 <= 37 )
+
+	{
+		printf ("%d\n", i);
+
+		i++;
+	}
+
+	return (0);
+
+}
---
+++ world/world.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main()
+{
+	int i;
+
+	int dest;
+
+	for ( int i=1;int dest=20; i<=dest; i++)
+	{
+		printf("Hello World\n");
+	}
+
+	return (0);
+
+}
